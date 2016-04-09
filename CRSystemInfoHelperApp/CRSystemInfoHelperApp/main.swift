f186727f574fa41960c2c9d70e91f1dccccfcca8
Cătălin Stan <catalin.stan@me.com>
2016-04-09 23:17:05 +0200

    Created embedded app

---
+++ CRSystemInfoHelperApp/CRSystemInfoHelperApp/main.swift
@@ -0,0 +1,54 @@
+//
+//  main.swift
+//  CRSystemInfoHelperApp
+//
+//  Created by Cătălin Stan on 09/04/16.
+//  Copyright © 2016 Cătălin Stan. All rights reserved.
+//
+
+import Foundation
+import CRSystemInfoHelper
+
+let helper:CRSystemInfoHelper = CRSystemInfoHelper.sharedHelper()
+
+//@property (nonatomic, readonly, strong) NSDictionary<NSString *, NSString *> * systemInfo;
+//@property (nonatomic, readonly, strong) NSString * systemInfoString;
+//@property (nonatomic, readonly, strong) NSString * systemVersionString;
+//@property (nonatomic, readonly) vm_size_t memoryUsage;
+//@property (nonatomic, readonly, strong) NSString * memoryUsageString;
+
+// IPv4 Addresses
+print("IPv4 Interfaces")
+for (ifname, ipaddr) in helper.AllIPAddresses {
+    print("\t\(ifname): \(ipaddr)")
+}
+print()
+
+// Main interface
+print("Main IP: \(helper.IPAddress)")
+print()
+
+// System info
+print("System Info (uname):")
+for (key, value) in helper.systemInfo {
+    print("\t\(key): \(value)")
+}
+print()
+
+// System Info String
+print("System Info String:")
+print(helper.systemInfoString)
+print()
+
+// System Version String
+print("System Version String:")
+print(helper.systemVersionString)
+print()
+
+// Memory Usage
+print("Memory Usage: \(helper.memoryUsage)")
+print()
+
+// Memory Usage String
+print("Memory Usage String: \(helper.memoryUsageString)")
+print()
